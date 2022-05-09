b00550122db6d98624c66c811c71141bed7b14b5
noe <noe@marleix.com>
2022-05-09 22:14:37 +0200

    all admin endpoint created

--- controllers/admin.controller.ts
+++ controllers/admin.controller.ts
@@ -1,9 +1,9 @@
 import express, {Request, Response, Router} from "express";
 import {AuthService, CoffeeService} from "../services";
 import {checkUserConnected} from "../middlewares";
-import {BigBossService} from "../services/bigboss.service";
+import {AdminService} from "../services/admin.service";
 
-export class BigBossControllerController {
+export class AdminController {
 
     async createResto(req: Request, res: Response) {
         console.log("begening create resto");
@@ -14,7 +14,7 @@ export class BigBossControllerController {
         }
         console.log("before try");
         try {
-            const user = await BigBossService.getInstance().createResto({
+            const user = await AdminService.getInstance().createResto({
                 name: req.body.name,
                 adress: req.body.adress,
                 menu: req.body.menu,
@@ -35,7 +35,7 @@ export class BigBossControllerController {
             return;
         }
         try {
-            const user = await BigBossService.getInstance().createEmployee({
+            const user = await AdminService.getInstance().createEmployee({
                 login: req.body.login,
                 password: req.body.password,
                 role: req.body.role,
@@ -49,7 +49,7 @@ export class BigBossControllerController {
 
     async deleteAdmin(req: Request, res: Response) {
         try {
-            const success = await BigBossService.getInstance().deleteAdminById(req.params.admin_id);
+            const success = await AdminService.getInstance().deleteAdminById(req.params.admin_id);
             if(success) {
                 res.status(204).end();
             } else {
@@ -66,7 +66,7 @@ export class BigBossControllerController {
             return;
         }
         try {
-            const admin = await BigBossService.getInstance().affectRestoToAdmin(req.body.resto_id,req.body.admin_id);
+            const admin = await AdminService.getInstance().affectRestoToAdmin(req.body.resto_id,req.body.admin_id);
             if (!admin) {
                 res.status(404).end();
                 return;
@@ -79,7 +79,7 @@ export class BigBossControllerController {
 
     async updateAdmin(req: Request, res: Response) {
         try {
-            const admin = await BigBossService.getInstance().updateAdminById(req.params.admin_id, req.body);
+            const admin = await AdminService.getInstance().updateAdminById(req.params.admin_id, req.body);
             if (!admin) {
                 res.status(404).end();
                 return;
@@ -92,7 +92,7 @@ export class BigBossControllerController {
 
     async getAdminById(req: Request, res: Response) {
         try {
-            const admin = await BigBossService.getInstance().getuserByIdAndRole(req.params.admin_id,"Admin");
+            const admin = await AdminService.getInstance().getuserByIdAndRole(req.params.admin_id,"Admin");
             if(admin === null) {
                 res.status(404).end();
                 return;
@@ -105,13 +105,13 @@ export class BigBossControllerController {
     }
 
     async getAllAdmin(req: Request, res: Response) {
-        const admins = await BigBossService.getInstance().getAllAdmin();
+        const admins = await AdminService.getInstance().getAllAdmin();
         res.json(admins);
     }
 
     async deleteResto(req: Request, res: Response) {
         try {
-            const success = await BigBossService.getInstance().deleteRestoById(req.params.resto_id);
+            const success = await AdminService.getInstance().deleteRestoById(req.params.resto_id);
             if(success) {
                 res.status(204).end();
             } else {
@@ -124,7 +124,7 @@ export class BigBossControllerController {
 
     async getResto(req: Request, res: Response) {
         try {
-            const resto = await BigBossService.getInstance().getRestoById(req.params.resto_id);
+            const resto = await AdminService.getInstance().getRestoById(req.params.resto_id);
             if(resto === null) {
                 res.status(404).end();
                 return;
@@ -137,16 +137,16 @@ export class BigBossControllerController {
     }
 
     async getAllResto(req: Request, res: Response) {
-        const resto = await BigBossService.getInstance().getAllResto();
+        const resto = await AdminService.getInstance().getAllResto();
         res.json(resto);
     }
 
-    async swapBigBoss(req: Request, res: Response) {
+    async swapAdmin(req: Request, res: Response) {
         if(!req.user){
             res.status(400).end();
             return;
         }
-        const Boss = await BigBossService.getInstance().swapBoss(req.user,req.params.admin_id);
+        const Boss = await AdminService.getInstance().swapBoss(req.user,req.params.admin_id);
         res.json(Boss);
     }
 
@@ -154,7 +154,7 @@ export class BigBossControllerController {
         console.log("test before try");
         try {
             console.log("test si on rentre dans le try");
-            const resto = await BigBossService.getInstance().updateById(req.params.resto_id, req.body);
+            const resto = await AdminService.getInstance().updateById(req.params.resto_id, req.body);
             if (!resto) {
                 res.status(404).end();
                 return;
@@ -172,24 +172,31 @@ export class BigBossControllerController {
 
     buildRoutes(): Router {
         const router = express.Router();
-        router.use(checkUserConnected("BigBoss"));
-        router.post('/createResto', express.json(), this.createResto.bind(this)); // permet d'ajouter un nouveau resto
-        router.delete('/deleteResto/:resto_id', this.deleteResto.bind(this)); // permet de delete un resto
-        router.get('/getResto/:resto_id', this.getResto.bind(this)); // permet d'afficher un resto
-        router.put('/updateResto/:resto_id', express.json(), this.updateResto.bind(this)); // permet d'update un resto
-        router.get('/getAllResto', this.getAllResto.bind(this)); // permet d'afficher tous les restos
+        router.use(checkUserConnected("Admin"));
+        router.post('/addProduct', express.json(), this.createResto.bind(this)); // permet d'ajouter un produit au menu
+        router.delete('/deleteProduct/:product_id', this.deleteResto.bind(this)); // permet de delete un produit
+        router.put('/updateProduct/:product_id', express.json(), this.updateResto.bind(this)); // permet d'update un produit
+        router.get('/getProduct/:product_id', this.getAllResto.bind(this)); // permet d'afficher un produit
+        router.get('/getAllProducts', this.getAllResto.bind(this)); // permet d'afficher tous les produits
+        
+        router.post('/addMenu', express.json(), this.createMenu.bind(this)); // permet de creer un compte admin
+        router.get('/getMenu/:menu_id', this.getMenuById.bind(this)); // permet d'afficher un admin
+        router.get('/getAllMenu', this.getAllMenu.bind(this)); // permet d'afficher tous les admins
+        router.put('/updateMenu/:menu_id', express.json(), this.updateMenu.bind(this)); // update admin
+        router.delete('/deleteMenu/:menu_id', this.deleteMenu.bind(this)); // permet de supp un compte admin
 
-        router.post('/subscribeAdmin', express.json(), this.createAdmin.bind(this)); // permet de creer un compte admin
-        router.get('/getAdmin/:admin_id', this.getAdminById.bind(this)); // permet d'afficher un admin
-        router.get('/getAllAdmin', this.getAllAdmin.bind(this)); // permet d'afficher tous les admins
-        router.put('/updateAdmin/:admin_id', express.json(), this.updateAdmin.bind(this)); // update admin
-        router.delete('/deleteAdmin/:admin_id', this.deleteAdmin.bind(this)); // permet de supp un compte admin
+        router.post('/addPromo', express.json(), this.createPromo.bind(this)); // permet d'ajouter une promo au menu
+        router.delete('/deletePromo/:promo_id', this.deletePromo.bind(this)); // permet de delete une promo
+        router.put('/updatePromo/:promo_id', express.json(), this.updatePromo.bind(this)); // permet d'update une promo
+        router.get('/getPromo/:promo_id', this.getAllPromo.bind(this)); // permet d'afficher une promo
+        router.get('/getAllPromos', this.getAllPromo.bind(this)); // permet d'afficher toutes les promos
 
-        router.put('/affectAdminToResto', express.json(), this.restoToAdmin.bind(this)); // affecte un admin a un resto
+        router.post('/subscribeOrder', express.json(), this.createOrder.bind(this)); // permet de creer un compte Order
+        router.get('/getOrder/:order_id', this.getOrderById.bind(this)); // permet d'afficher un Order
+        router.get('/getAllOrder', this.getAllOrder.bind(this)); // permet d'afficher tous les Orders
+        router.put('/updateOrder/:order_id', express.json(), this.updateOrder.bind(this)); // update Order
+        router.delete('/deleteOrder/:order_id', this.deleteOrder.bind(this)); // permet de supp un compte Order
 
-        router.get('/whoAmI', this.me.bind(this)); // who am i
-
-        router.post('/newBigBoss/:admin_id', express.json(), this.swapBigBoss.bind(this)); // change de big boss
         return router;
     }
 }
---
+++ services/admin.service.ts
@@ -0,0 +1,193 @@
+import {possibleRole, Role, UserDocument, UserModel, UserProps} from "../models";
+import {AuthUtils, SecurityUtils} from "../utils";
+import {SessionDocument, SessionModel} from "../models/session.model";
+import {Session} from "inspector";
+import {RestoDocument, RestoModel, RestoProps} from "../models/restau.model";
+import {CoffeeDocument, CoffeeModel, CoffeeProps} from "../models/coffee.model";
+
+export class AdminService {
+
+    private static instance?: AdminService;
+
+    public static getInstance(): AdminService {
+        if(AdminService.instance === undefined) {
+            AdminService.instance = new AdminService();
+        }
+        return AdminService.instance;
+    }
+
+    private constructor() { }
+
+    public async createResto(props: RestoProps): Promise<RestoDocument> {
+        const model = new RestoModel(props);
+        const resto = await model.save();
+        return resto;
+    }
+
+    public async createEmployee(user:Partial<UserProps>):Promise<UserDocument>{
+        if(!user.password) {
+            throw new Error('Missing password');
+        }
+        if(!user.role) {
+            throw new Error('Missing role');
+        }
+        if(!(user.role in Role) || user.role==="Admin"){
+            throw new Error('Bad role');
+        }
+        const model = new UserModel({
+            login: user.login,
+            password: SecurityUtils.sha512(user.password),
+            role:user.role
+
+        });
+        return model.save();
+    }
+
+    async deleteRestoById(restoId: string): Promise<boolean> {
+        const res = await RestoModel.deleteOne({_id: restoId}).exec();
+        return res.deletedCount === 1;
+    }
+    async deleteAdminById(adminId: string): Promise<boolean> {
+        const res = await UserModel.deleteOne({$and:[{_id:adminId},{ role :"Admin"}]}).exec();
+        return res.deletedCount === 1;
+    }
+
+    async getuserByIdAndRole(userId: string,role:string=""): Promise<UserDocument | null> {
+        const user = await UserModel.findById(userId).exec();
+        if(!user){
+            return null;
+        }
+        if(role===""){
+            return user
+        }else {
+            if (user.role === role) {
+                return user;
+            } else {
+                return null;
+            }
+        }
+
+    }
+    async getRestoById(restoId: string): Promise<RestoDocument | null> {
+        return RestoModel.findById(restoId).exec();
+    }
+
+    async getAllResto(): Promise<RestoDocument[]> {
+        return RestoModel.find().exec();
+    }
+
+    async affectRestoToAdmin(restoId: string,adminId: string): Promise<UserDocument | null> {
+        const resto = await this.getRestoById(restoId);
+        const admin = await this.getuserByIdAndRole(adminId,"Admin");
+        if(!admin || !resto) {
+            return null;
+        }
+        if(resto._id!==undefined){
+            admin.restaurant=resto._id;
+        }
+        const res = await admin.save();
+        return res;
+
+    }
+
+    async swapBoss(pastBoss: UserProps,newBoss: string){
+        const newB = await this.getuserByIdAndRole(newBoss,"Admin");
+        if (!pastBoss || !newB) {
+            return null;
+        }
+        const past= await UserModel.find({
+            role: "Admin"
+        }).exec();
+        if( past.length!==1){
+            return null;
+        }
+        newB.role=possibleRole["Admin"]
+        past[0].role=possibleRole["Admin"]
+        const res1 = await newB.save();
+        const res2 = await past[0].save();
+        return {res1,res2};
+    }
+
+    async updateAdminById(adminId: string, props: Partial<UserProps>): Promise<UserDocument | null> {
+        const admin = await this.getuserByIdAndRole(adminId,"Admin");
+        if (!admin) {
+            return null;
+        }
+        if(props.login !== undefined) {
+            admin.login = props.login;
+        }
+        if(props.password !== undefined) {
+            admin.password = SecurityUtils.sha512(props.password);
+        }
+        if(props.restaurant !== undefined) {
+            admin.restaurant = props.restaurant;
+        }
+        if(props.role !== undefined && props.role in Role) {
+            admin.role = props.role;
+        }
+        const res = await admin.save();
+        return res;
+
+    }
+    async getAllAdmin(): Promise<UserDocument[]> {
+        return UserModel.find({role: "Admin"}).exec();
+    }
+    async updateById(restoId: string, props: RestoProps): Promise<RestoDocument | null> {
+        const resto = await this.getRestoById(restoId);
+        if(!resto) {
+            return null;
+        }
+        if(props.name !== undefined) {
+            resto.name = props.name;
+        }
+        if(props.adress !== undefined) {
+            resto.adress = props.adress;
+        }
+        if(props.menu !== undefined) {
+            resto.menu = props.menu;
+        }
+        if(props.promotion !== undefined) {
+            resto.promotion = props.promotion;
+        }
+        if(props.product !== undefined) {
+            resto.product = props.product;
+        }
+        if(props.admin !== undefined) {
+            resto.admin = props.admin;
+        }
+        const res = await resto.save();
+        return res;
+    }
+
+    // Pick selectionne des champs dans le type
+    public async logIn(info: Pick<UserProps, 'login' | 'password'>, platform: string): Promise<SessionDocument | null> {
+        const user = await UserModel.findOne({
+            login: info.login,
+            password: SecurityUtils.sha512(info.password)
+        }).exec();
+        if(user === null) {
+            throw new Error('User not found');
+        }
+        // 604_800 -> 1 week in seconds
+        const currentDate = new Date();
+        const expirationDate = new Date(currentDate.getTime() + 604_800_000);
+        const session = await SessionModel.create({
+            platform,
+            expiration: expirationDate,
+            user: user._id
+        });
+        user.sessions.push(session._id); // permet de memoriser la session dans le user
+        await user.save();
+        return session;
+    }
+
+    public async getUserFrom(token: string): Promise<UserProps | null> {
+        const session = await SessionModel.findOne({
+            _id: token,
+            expiration: {
+                $gte: new Date()
+            }
+        }).populate("user").exec();
+        return session ? session.user as UserProps : null;
+    }
+}
