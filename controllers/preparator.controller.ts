f24b5b47dda7491541fa56b0fb6ab04cacb5c0d5
noe <noe@marleix.com>
2022-07-16 15:56:49 +0200

    get interventions before and after

--- controllers/preparator.controller.ts
+++ controllers/preparator.controller.ts
@@ -31,11 +31,48 @@ export class PreparatorController {
         }
     }
 
+    async getAllInterventionsAfter(req: Request, res: Response) {
+        const body = req.body;
+        const date = body.date;
+        console.log("coucou", date);
+        if(req.headers.authorization){
+            const tmpUser = await this.getUserByTokenSession(req.headers.authorization);
+            let id;
+            if(tmpUser && tmpUser.id)
+                id = tmpUser.id
+            try {
+                const interventions = await PreparatorService.getInstance().getAllInterventionsAfter(id);
+                res.json(interventions);
+
+            } catch(err) {
+                res.status(400).end();
+            }
+        }
+    }
+
+    async getAllInterventionsBefore(req: Request, res: Response) {
+        if(req.headers.authorization){
+            const tmpUser = await this.getUserByTokenSession(req.headers.authorization);
+            let id;
+            if(tmpUser && tmpUser.id)
+                id = tmpUser.id
+            try {
+                const interventions = await PreparatorService.getInstance().getAllInterventionsBefore(id);
+                res.json(interventions);
+
+            } catch(err) {
+                res.status(400).end();
+            }
+        }
+    }
+
     buildRoutes(): Router {
         const router = express.Router();
         //router.use();
         router.use(checkUserConnected(""));
         router.get('/interventions', this.getAllInterventions.bind(this));
+        router.get('/interventionsAfter', express.json(), this.getAllInterventionsAfter.bind(this));
+        router.get('/interventionsBefore',express.json(), this.getAllInterventionsBefore.bind(this));
         return router;
     }
 }
--- services/preparator.service.ts
+++ services/preparator.service.ts
@@ -1,7 +1,9 @@
 import {CoffeeDocument, CoffeeModel, CoffeeProps} from "../models/coffee.model";
 import {InterventionDocument, InterventionModel} from "../models/intervention.model";
+import {Schema} from "mongoose";
 export class PreparatorService {
     private static instance?: PreparatorService;
+    private izi: any;
     public static getInstance(): PreparatorService {
         if(PreparatorService.instance === undefined) {
             PreparatorService.instance = new PreparatorService();
@@ -20,6 +22,34 @@ export class PreparatorService {
         return InterventionModel.find({
             idCustomer: id
         }).exec();
+    }
+
+    async getAllInterventionsAfter(id: string): Promise<InterventionDocument[]> {
+        let tmp =  await InterventionModel.find({
+            idCustomer: id
+        }).exec();
+        console.log("tmp : ", tmp);
+        let interventions = [];
+        var current = new Date();
+        for(let i = 0; i < tmp.length; i++){
+            if(tmp[i].datePlanned > current)
+                interventions.push(tmp[i]);
+        }
+        console.log("interventions : ", interventions);
+        return interventions;
+    }
+
+    async getAllInterventionsBefore(id: string): Promise<InterventionDocument[]> {
+        let tmp =  await InterventionModel.find({
+            idCustomer: id
+        }).exec();
+        let interventions = [];
+        var current = new Date();
+        for(let i = 0; i < tmp.length; i++){
+            if(tmp[i].datePlanned < current)
+                interventions.push(tmp[i]);
+        }
+        return interventions;
     }
 
     async getById(coffeeId: string): Promise<CoffeeDocument | null> {
